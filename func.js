c57045e5be3de66659cb4bc7695d6b29de577a56
tomekun <154856233+tomekun@users.noreply.github.com>
2024-08-09 02:18:10 +0000

    更新

--- func.js
+++ func.js
@@ -142,57 +142,7 @@ function surveillance() {
 
 
 }
-function getServerInformation(guild) {
-  const channels = guild.channels.cache;
-  const serverData = [];
-  const japanTime = new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
-  const japanDate = new Date(japanTime);
-  var synthesisTime =　`${japanDate.getHours()}:${japanDate.getMinutes()}:${japanDate.getSeconds()}:${japanDate.getMilliseconds()}`
-  var synthesisDate = `${japanDate.getMonth()+1}/${japanDate.getDate()}-${synthesisTime}` 
 
-  channels.forEach((channel) => {
-    if (channel.type === 4) {
-      const categoryData = {
-        //time: channel.createdAt.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }),
-        time:synthesisDate,
-        name: channel.name,
-        id: channel.id,
-        type: 4,
-        channels: [],
-      };
-
-      channels.forEach((subChannel) => {
-        if (subChannel.parentId === channel.id) {
-          categoryData.channels.push({
-            name: subChannel.name,
-            id: subChannel.id,
-            type: subChannel.type === 0 ? 0 : 2,
-          });
-        }
-      });
-
-      serverData.push(categoryData);
-    }
-  });
-
-  const uncategorizedChannels = channels.filter(channel => !channel.parent);
-  uncategorizedChannels.forEach(channel => {
-    if (channel.type != 4 ){ 
-    serverData.push({
-      name: channel.name,
-      id: channel.id,
-      type: channel.type === 0 ? 0 : 2,
-    });
-    }
-  });
-
-  const serverDataJson = JSON.stringify(serverData, null, 2);
-  const jsonFileName = `./commands/ServerData/serverData:${synthesisTime}.json`;
-  fs.writeFileSync(jsonFileName, JSON.stringify(serverData, null, 2));
-  console.log('チャンネルとカテゴリー情報をJSONファイルに保存しました.');
-
-  return serverDataJson;
-}
 module.exports = {
   surveillance,
   getServerInformation,

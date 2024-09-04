900374772a0087cc0a47acec875386f9322f4cce
tomekun <154856233+tomekun@users.noreply.github.com>
2024-09-04 20:48:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -214,6 +214,7 @@ function handleMentions(message, userId, currentTime) {
 async function handleInviteLinks(message, userId) {
   
   const urls = message.content.match(urlRegex);
+  if(urls.inculudes("https://discord.com/channels/1207931136211161108"))return;
  
   if (urls){   
   for (const url of urls) {
